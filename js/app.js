239966cf5570bd771aac4e5f78f7f620a2bd1bb6
Alex Santos <alex-ps@live.com>
2024-08-30 00:34:36 -0300

    finalizando desafio

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-//teste
+function alterarStatus(Id) {
+    let gameClicado = document.getElementById("game-"+Id);
+    let imgaem = gameClicado.querySelector(".dashboard__item__img");
+    let botao = gameClicado.querySelector(".dashboard__item__button");
+
+    if (imgaem.classList.contains("dashboard__item__img--rented")){
+            imgaem.classList.remove("dashboard__item__img--rented")
+            botao.classList.remove("dashboard__item__button--return");
+            botao.textContent = "Alugar";
+
+    } else {
+            imgaem.classList.add("dashboard__item__img--rented");
+            botao.classList.add("dashboard__item__button--return");
+            botao.textContent = "Devolver";
+    }
+
+
+}
